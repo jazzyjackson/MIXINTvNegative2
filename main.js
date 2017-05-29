b162176bcd08aaf415ebff5eea533d88c32ff80f
Colten Jackson <Colten Jackson>
2017-05-29 02:15:10 -0400

    Just listening for GET requests so far

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
 const Koa = require('koa')
+const fs = require('fs')
 const app = new Koa()
 
-app.use(require('./editor/metaparser'))
-app.use(require('./root/metaparser'))
+app.use(async (context, next) => {
+  context.body = await ({
+    'GET': function(){
+        return new Promise((resolve, reject) => fs.readFile('.' + context.path, (err, data) => {
+          if (err) reject (err)
+          else resolve(data.toString())
+        }))
+    },
+    'POST': () => {
+      console.log('posted')
+    },
+    'DELETE': () => {
+      console.log('deleted')
+    },
+    'PUT': () => {
+      console.log('putted')
+    },
+  })[context.method]()
+})
+
+
 app.listen(3000)
